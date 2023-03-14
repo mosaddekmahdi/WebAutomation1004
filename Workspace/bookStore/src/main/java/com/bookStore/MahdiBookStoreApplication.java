799695114acd9cd9d0b444451ae28318743074c5
Mosaddek Mahdi <34359380+mosaddekmahdi@users.noreply.github.com>
2023-03-14 12:01:45 -0400

    book store project

---
+++ Workspace/bookStore/src/main/java/com/bookStore/MahdiBookStoreApplication.java
@@ -0,0 +1,14 @@
+package com.bookStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahdiBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MahdiBookStoreApplication.class, args);
+		
+	}
+
+}
